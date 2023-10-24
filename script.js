e89c0680f0eafff2e5bf2dc8add5ca7a0a988683
dessties <dessties@gmail.com>
2023-10-24 16:14:29 +0300

    final commit

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 window.onload = function () {
+    let color2 = randomColor();
+    let rgbColor2 = `rgb(${color2.r}, ${color2.g}, ${color2.b})`;
+    document.getElementById("bg").style.backgroundColor = rgbColor2;
+    
     document.getElementById("openWindow").classList.add("appear");
     let color1 = randomColor();
     let rgbColor1 = `rgb(${color1.r}, ${color1.g}, ${color1.b})`;
@@ -10,6 +14,7 @@ let isFirstClick = true;
 document.body.addEventListener("click", () => {
     if (isFirstClick) {
         openWindow.classList.remove("appear");
+
         document.getElementById("openWindow").classList.add("disappear");
         isFirstClick = false;
     }
